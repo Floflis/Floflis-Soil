2ce73b4550330a31283b970be071a354b6de3455
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-05-17 19:41:54 -0300

    to-merge.sh: implement maysudo

--- to-merge.sh
+++ to-merge.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+is_root=false
+
+if [ "$([[ $UID -eq 0 ]] || echo "Not root")" = "Not root" ]
+   then
+      is_root=false
+   else
+      is_root=true
+fi
+
+$maysudo=""
+
+if [ "$is_root" = "false" ]
+   then
+      $maysudo="sudo"
+   else
+      $maysudo=""
+fi
+
 cat > /usr/share/applications/ipfs-handle-link.desktop <<EOF
 [Desktop Entry]
 Type=Application
@@ -28,7 +46,7 @@ cat > /usr/bin/uniswap <<EOF
 
 xdg-open ipfs://uniswap.eth
 EOF
-sudo chmod +x /usr/bin/uniswap
+$maysudo chmod +x /usr/bin/uniswap
 cat > /usr/share/applications/uniswap.desktop <<EOF
 [Desktop Entry]
 Encoding=UTF-8
@@ -46,7 +64,7 @@ cat > /usr/bin/decentraland <<EOF
 
 xdg-open https://play.decentraland.org/
 EOF
-sudo chmod +x /usr/bin/decentraland
+$maysudo chmod +x /usr/bin/decentraland
 cat > /usr/share/applications/decentraland.desktop <<EOF
 [Desktop Entry]
 Encoding=UTF-8
@@ -64,7 +82,7 @@ cat > /usr/bin/thesandbox <<EOF
 
 xdg-open https://www.sandbox.game/en/
 EOF
-sudo chmod +x /usr/bin/thesandbox
+$maysudo chmod +x /usr/bin/thesandbox
 cat > /usr/share/applications/thesandbox.desktop <<EOF
 [Desktop Entry]
 Encoding=UTF-8
@@ -77,13 +95,13 @@ Categories=Game;Simulation;Metaverse;Ethereum;
 Keywords=metaverse;world;mining;tokens;ethereum;wearables;multiplayer;roleplaying;sandbox;voxels;
 EOF
 
-sudo snap install mmex
+$maysudo snap install mmex
 
-sudo add-apt-repository ppa:openshot.developers/ppa && sudo apt-get update && sudo apt-get install openshot-qt
+$maysudo add-apt-repository ppa:openshot.developers/ppa && sudo apt-get update && sudo apt-get install openshot-qt
 
-sudo apt install minetest
+$maysudo apt install minetest
 
 #- Floflis main Ubuntu ISO will use Ultimate layer. For Home layer, different ISO base: https://help.ubuntu.com/community/Installation/MinimalCD
 
-sudo sed -i 's/^PRETTY_NAME=" .*$/PRETTY_NAME=" Floflis 20 build 2105 'Eusoumafoca'"/' /usr/lib/os-release
-sudo sed -i 's/^DISTRIB_DESCRIPTION=" .*$/DISTRIB_DESCRIPTION=" Floflis 20 build 2105 'Eusoumafoca'"/' /etc/lsb-release
+$maysudo sed -i 's/^PRETTY_NAME=" .*$/PRETTY_NAME=" Floflis 20 build 2105 'Eusoumafoca'"/' /usr/lib/os-release
+$maysudo sed -i 's/^DISTRIB_DESCRIPTION=" .*$/DISTRIB_DESCRIPTION=" Floflis 20 build 2105 'Eusoumafoca'"/' /etc/lsb-release
