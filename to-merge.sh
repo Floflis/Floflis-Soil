df8f08de91cbaca6e4777d63ba731a6e0f5f39df
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-06-17 14:39:18 -0300

    to-merge.sh: change icon of File Explorer (Nemo), add mimetypes for .apps, .game and .html5

--- to-merge.sh
+++ to-merge.sh
@@ -400,6 +400,7 @@ if [ -e /usr/share/icons/Yaru ]; then
        cd /usr/share/icons/ubuntu/Yaru/apps
        rm -f org.gnome.baobab.png && ln -s disk-usage-app.png baobab.png && ln -s disk-usage-app.png org.gnome.baobab.png && rm -f bijiben.png && rm -f org.gnome.bijiben.png && rm -f org.gnome.Bijiben.png && rm -f org.gnome.Notes.png && ln -s notes-app.png bijiben.png && ln -s notes-app.png org.gnome.bijiben.png && ln -s notes-app.png org.gnome.Bijiben.png && ln -s notes-app.png org.gnome.Notes.png && rm -f dconf-editor.png && rm -f ca.desrt.dconf-editor.png && ln -s configurator-app.png dconf-editor.png && ln -s configurator-app.png ca.desrt.dconf-editor.png && rm -f gnome-clocks.png && rm -f org.gnome.clocks.png && rm -f org.gnome.Clocks.png && ln -s clock-app.png gnome-clocks.png && ln -s clock-app.png org.gnome.clocks.png && ln -s clock-app.png org.gnome.Clocks.png && rm -f gnome-disks.png && rm -f org.gnome.Disks.png && rm -f org.gnome.DiskUtility.png && ln -s disk-utility-app.png gnome-disks.png && ln -s disk-utility-app.png org.gnome.Disks.png && ln -s disk-utility-app.png org.gnome.DiskUtility.png && rm -f gnome-documents.png && rm -f org.gnome.Documents.png && ln -s documents-app.png gnome-documents.png && ln -s documents-app.png org.gnome.Documents.png && rm -f org.gnome.Books.png && ln -s ebook-reader-app.png org.gnome.Books.png && rm -f empathy.png && rm -f internet-chat.png && rm -f org.gnome.Empathy.png && ln -s messaging-app.png empathy.png && ln -s messaging-app.png internet-chat.png && ln -s messaging-app.png org.gnome.Empathy.png && rm -f eog.png && rm -f multimedia-photo-viewer.png && rm -f org.gnome.eog.png && ln -s image-viewer-app.png eog.png && ln -s image-viewer-app.png multimedia-photo-viewer.png && ln -s image-viewer-app.png org.gnome.eog.png && rm -f org.gnome.Evince.png && ln -s evince.png org.gnome.Evince.png
        rm -f gnome-screenshot.png && rm -f org.gnome.Screenshot.png && rm -f screenshot-app.png && ln -s applets-screenshooter.png gnome-screenshot.png && ln -s applets-screenshooter.png org.gnome.Screenshot.png && ln -s applets-screenshooter.png screenshot-app.png && rm -f gnome-characters.png && rm -f org.gnome.Characters.png && ln -s accessories-character-map.png gnome-characters.png && ln -s accessories-character-map.png org.gnome.Characters.png && rm -f gedit.png && rm -f org.gnome.gedit.png && rm -f org.gnome.Gedit.png && ln -s accessories-text-editor.png gedit.png && ln -s accessories-text-editor.png org.gnome.gedit.png && ln -s accessories-text-editor.png org.gnome.Gedit.png && rm -f gnome-contacts.png && rm -f office-address-book.png && rm -f office-addressbook.png && rm -f org.gnome.Contacts.png && rm -f x-office-address-book.png && ln -s address-book-app.png gnome-contacts.png && ln -s address-book-app.png office-address-book.png && ln -s address-book-app.png office-addressbook.png && ln -s address-book-app.png org.gnome.Contacts.png && ln -s address-book-app.png x-office-address-book.png && rm -f apport.png && ln -s error-app.png apport.png && rm -f packages-app.png && ln -s system-software-install.png packages-app.png && rm -f deja-dup.png && rm -f org.gnome.DejaDup.png && ln -s backups-app.png deja-dup.png && ln -s backups-app.png org.gnome.DejaDup.png && rm -f baobab.png
+       cd
 fi
 
 #ipfs add $(ethereal ens contenthash get --domain=1inch.eth)
@@ -449,3 +450,373 @@ fi
     *)
        echo "${invalid}" ;;
  esac
+ 
+$maysudo cat > /usr/share/applications/csd-automount.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Name=Files
+Name[am]=ፋይሎች
+Name[bg]=Файлове
+Name[ca]=Arxius
+Name[cs]=Soubory
+Name[da]=Filer
+Name[de]=Dateien
+Name[es]=Archivos
+Name[eu]=Fitxategiak
+Name[fr]=Fichiers
+Name[hr]=Nemo
+Name[hu]=Fájlok
+Name[ko]=파일
+Name[lt]=Failai
+Name[nl]=Bestanden
+Name[pl]=Pliki
+Name[pt]=Ficheiros
+Name[pt_BR]=Arquivos
+Name[pt_PT]=Ficheiros
+Name[ru]=Файлы
+Name[sv]=Filer
+Name[tr]=Dosyalar
+Name[uk]=Файли
+Name[zh_CN]=文件
+Name[zh_HK]=檔案
+Icon=system-file-manager
+Exec=/usr/lib/x86_64-linux-gnu/cinnamon-settings-daemon/csd-automount
+OnlyShowIn=X-Cinnamon;
+NoDisplay=true
+EOF
+
+$maysudo cat > /usr/share/applications/nemo.desktop <<EOF
+[Desktop Entry]
+Name=Files
+Name[am]=ፋይሎች
+Name[ar]=الملفات
+Name[bg]=Файлове
+Name[ca]=Fitxers
+Name[ca@valencia]=Fitxers
+Name[cs]=Soubory
+Name[cy]=Ffeiliau
+Name[da]=Filer
+Name[de]=Dateien
+Name[el]=Αρχεία
+Name[eo]=Dosieroj
+Name[es]=Archivos
+Name[et]=Failid
+Name[eu]=Fitxategiak
+Name[fi]=Tiedostot
+Name[fr]=Fichiers
+Name[fr_CA]=Fichiers
+Name[he]=קבצים
+Name[hr]=Nemo
+Name[hu]=Fájlok
+Name[id]=Berkas
+Name[is]=Skrár
+Name[kab]=Ifuyla
+Name[ko]=파일
+Name[lt]=Failai
+Name[nl]=Bestanden
+Name[pl]=Pliki
+Name[pt]=Ficheiros
+Name[pt_BR]=Arquivos
+Name[ro]=Fișiere
+Name[ru]=Файлы
+Name[sk]=Súbory
+Name[sl]=Datoteke
+Name[sr]=Датотеке
+Name[sr@latin]=Датотеке
+Name[sv]=Filer
+Name[th]=แฟ้ม
+Name[tr]=Dosyalar
+Name[uk]=Файли
+Name[zh_CN]=文件
+Name[zh_HK]=檔案
+Comment=Access and organize files
+Comment[am]=ፋይሎች ጋር መድረሻ እና ማደራጃ
+Comment[ar]=الوصول إلى الملفات وتنظيمها
+Comment[bg]=Достъп и управление на файлове
+Comment[ca]=Organitzeu i accediu als fitxers
+Comment[ca@valencia]=Organitzeu i accediu als fitxers
+Comment[cs]=Přístup k souborům a jejich správa
+Comment[cy]=Mynediad i drefnu ffeiliau
+Comment[da]=Tilgå og organisér filer
+Comment[de]=Dateien aufrufen und organisieren
+Comment[el]=Πρόσβαση και οργάνωση αρχείων
+Comment[en_GB]=Access and organise files
+Comment[eo]=Atingi kaj organizi dosierojn
+Comment[es]=Acceder a los archivos y organizarlos
+Comment[et]=Ligipääs failidele ning failipuu korrastamine
+Comment[eu]=Atzitu eta antolatu fitxategiak
+Comment[fi]=Avaa ja järjestä tiedostoja
+Comment[fr]=Accéder aux fichiers et les organiser
+Comment[fr_CA]=Accéder aux fichiers et les organiser
+Comment[he]=גישה לקבצים וארגונם
+Comment[hr]=Pristupite i organizirajte datoteke
+Comment[hu]=Fájlok elérése és rendszerezése
+Comment[ia]=Acceder e organisar le files
+Comment[id]=Akses dan kelola berkas
+Comment[ie]=Accesse e ordina files
+Comment[is]=Aðgangur og skipulag skráa
+Comment[it]=Accede ai file e li organizza
+Comment[kab]=Kcem udiɣ suddes ifuyla
+Comment[ko]=파일 접근 및 정리
+Comment[lt]=Gauti prieigą prie failų ir juos tvarkyti
+Comment[nl]=Bestanden gebruiken en organiseren
+Comment[pl]=Porządkowanie i dostęp do plików
+Comment[pt]=Aceder e organizar ficheiros
+Comment[pt_BR]=Acesse e organize arquivos
+Comment[ro]=Accesează și organizează fișiere
+Comment[ru]=Управление и доступ к файлам
+Comment[sk]=Prístup a organizácia súborov
+Comment[sl]=Dostop in razvrščanje datotek
+Comment[sr]=Приступите датотекама и организујте их
+Comment[sr@latin]=Приступите датотекама и организујте их
+Comment[sv]=Kom åt och organisera filer
+Comment[th]=เข้าถึงและจัดระเบียบแฟ้ม
+Comment[tr]=Dosyalara eriş ve düzenle
+Comment[uk]=Доступ до файлів та впорядковування файлів
+Comment[zh_CN]=访问和组织文件
+Comment[zh_HK]=存取與組織檔案
+Exec=nemo %U
+Icon=system-file-manager
+# Translators: these are keywords of the file manager
+Keywords=folders;filesystem;explorer;
+Terminal=false
+Type=Application
+StartupNotify=false
+Categories=GNOME;GTK;Utility;Core;
+MimeType=inode/directory;application/x-gnome-saved-search;
+Actions=open-home;open-computer;open-trash;
+
+[Desktop Action open-home]
+Name=Home
+Name[af]=Tuis
+Name[am]=ቤት
+Name[ar]=المجلد الرئيسي
+Name[be]=Дом
+Name[bg]=Домашна папка
+Name[bn]=হোম
+Name[bs]=Početni direktorij
+Name[ca]=Carpeta de l'usuari
+Name[ca@valencia]=Carpeta de l'usuari
+Name[cs]=Domov
+Name[cy]=Cartref
+Name[da]=Hjem
+Name[de]=Persönlicher Ordner
+Name[el]=Προσωπικός φάκελος
+Name[eo]=Hejmo
+Name[es]=Carpeta personal
+Name[et]=Kodu
+Name[eu]=Karpeta nagusia
+Name[fi]=Koti
+Name[fr]=Dossier personnel
+Name[fr_CA]=Dossier personnel
+Name[ga]=Baile
+Name[gd]=Dhachaigh
+Name[gl]=Cartafol persoal
+Name[he]=בית
+Name[hr]=Osobna mapa
+Name[hu]=Saját mappa
+Name[ia]=Al domo
+Name[id]=Beranda
+Name[ie]=Hem
+Name[is]=Heimamappa
+Name[ja]=ホーム
+Name[kab]=Agejdan
+Name[kk]=Үй
+Name[kn]=ಮನೆ
+Name[ko]=홈
+Name[ku]=Mal
+Name[lt]=Namai
+Name[ml]=ആസ്ഥാനം
+Name[mr]=मुख्य
+Name[ms]=Rumah
+Name[nb]=Hjem
+Name[nl]=Persoonlijke map
+Name[oc]=Dorsièr personal
+Name[pl]=Katalog domowy
+Name[pt]=Pasta Pessoal
+Name[pt_BR]=Pasta pessoal
+Name[ro]=Dosar personal
+Name[ru]=Домашняя папка
+Name[sk]=Domov
+Name[sl]=Domov
+Name[sr]=Почетна
+Name[sr@latin]=Početna
+Name[sv]=Hem
+Name[ta]=இல்லம்
+Name[tg]=Асосӣ
+Name[th]=บ้าน
+Name[tr]=Ev Dizini
+Name[uk]=Домівка
+Name[ur]=المنزل
+Name[vi]=Nhà
+Name[zh_CN]=主目录
+Name[zh_HK]=家
+Name[zh_TW]=家
+Exec=nemo %U
+
+[Desktop Action open-computer]
+Name=Computer
+Name[af]=Rekenaar
+Name[am]=ኮምፒዩተር
+Name[ar]=الكمبيوتر
+Name[ast]=Ordenador
+Name[be]=Кампутар
+Name[bg]=Компютър
+Name[bn]=কম্পিউটার
+Name[bs]=Računar
+Name[ca]=Ordinador
+Name[ca@valencia]=Ordinador
+Name[cs]=Počítač
+Name[cy]=Cyfrifiadur
+Name[de]=Rechner
+Name[el]=Υπολογιστής
+Name[eo]=Komputilo
+Name[es]=Equipo
+Name[et]=Arvuti
+Name[eu]=Ordenagailua
+Name[fi]=Tietokone
+Name[fr]=Poste de travail
+Name[fr_CA]=Poste de travail
+Name[gd]=Coimpiutair
+Name[gl]=Computador
+Name[he]=מחשב
+Name[hr]=Računalo
+Name[hu]=Számítógép
+Name[ia]=Computator
+Name[id]=Komputer
+Name[ie]=Computator
+Name[is]=Tölva
+Name[ja]=コンピュータ
+Name[kab]=Aselkim
+Name[kk]=Компьютер
+Name[kn]=ಗಣಕ
+Name[ko]=컴퓨터
+Name[ku]=Komputer
+Name[lt]=Kompiuteris
+Name[ml]=കമ്പ്യൂട്ടർ
+Name[mr]=संगणक
+Name[ms]=Komputer
+Name[nb]=Datamaskin
+Name[nn]=Datamaskin
+Name[oc]=Ordenador
+Name[pl]=Komputer
+Name[pt]=Computador
+Name[pt_BR]=Computador
+Name[ru]=Компьютер
+Name[sk]=Počítač
+Name[sl]=Računalnik
+Name[sq]=Kompjuteri
+Name[sr]=Рачунар
+Name[sr@latin]=Računar
+Name[sv]=Dator
+Name[ta]=கணினி
+Name[tg]=Компютер
+Name[th]=คอมพิวเตอร์
+Name[tr]=Bilgisayar
+Name[uk]=Комп’ютер
+Name[ur]=کمپیوٹر
+Name[vi]=Máy tính
+Name[zh_CN]=计算机
+Name[zh_HK]=電腦
+Name[zh_TW]=電腦
+Exec=nemo computer:///
+
+[Desktop Action open-trash]
+Name=Trash
+Name[af]=Asblik
+Name[am]=ቆሻሻ
+Name[ar]=سلة المهملات
+Name[ast]=Papelera
+Name[be]=Сметніца
+Name[bg]=Кошче
+Name[bn]=ট্র্যাশ
+Name[bs]=Smeće
+Name[ca]=Paperera
+Name[ca@valencia]=Paperera
+Name[cs]=Koš
+Name[cy]=Sbwriel
+Name[da]=Papirkurv
+Name[de]=Papierkorb
+Name[el]=Απορρίμματα
+Name[en_GB]=Rubbish Bin
+Name[eo]=Rubujo
+Name[es]=Papelera
+Name[et]=Prügi
+Name[eu]=Zakarrontzia
+Name[fi]=Roskakori
+Name[fr]=Corbeille
+Name[fr_CA]=Corbeille
+Name[ga]=Bruscar
+Name[gd]=An sgudal
+Name[gl]=Lixo
+Name[he]=אשפה
+Name[hr]=Smeće
+Name[hu]=Kuka
+Name[ia]=Immunditia
+Name[id]=Tempat sampah
+Name[ie]=Paper-corb
+Name[is]=Rusl
+Name[it]=Cestino
+Name[ja]=ゴミ箱
+Name[kab]=Iḍumman
+Name[kk]=Себет
+Name[kn]=ಕಸಬುಟ್ಟಿ
+Name[ko]=휴지통
+Name[ku]=Avêtî
+Name[lt]=Šiukšlinė
+Name[ml]=ട്രാഷ്
+Name[mr]=कचरापेटी
+Name[ms]=Tong Sampah
+Name[nb]=Papirkurv
+Name[nds]=Papierkorb
+Name[nl]=Prullenbak
+Name[nn]=Papirkorg
+Name[oc]=Escobilhièr
+Name[pl]=Kosz
+Name[pt]=Lixo
+Name[pt_BR]=Lixeira
+Name[ro]=Coș de gunoi
+Name[ru]=Корзина
+Name[sk]=Kôš
+Name[sl]=Smeti
+Name[sq]=Koshi
+Name[sr]=Смеће
+Name[sr@latin]=Kanta
+Name[sv]=Papperskorg
+Name[ta]=குப்பைத் தொட்டி
+Name[tg]=Сабад
+Name[th]=ถังขยะ
+Name[tr]=Çöp
+Name[uk]=Смітник
+Name[ur]=ردی
+Name[vi]=Thùng rác
+Name[zh_CN]=回收站
+Name[zh_HK]=垃圾桶
+Name[zh_TW]=回收筒
+Exec=nemo trash:///
+EOF
+
+$maysudo cat >> /etc/mime.types <<EOF
+application/x-html5			        html5
+application/x-apps			        apps
+application/x-game			        game
+EOF
+$maysudo cat > /usr/share/mime/packages/.xml <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="application/x-html5">
+<glob pattern="*.html5"/>
+</mime-type>
+EOF
+$maysudo cat > /usr/share/mime/packages/.xml <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="application/x-apps">
+<glob pattern="*.apps"/>
+</mime-type>
+EOF
+$maysudo cat > /usr/share/mime/packages/.xml <<EOF
+<?xml version="1.0" encoding="utf-8"?>
+<mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="application/x-game">
+<glob pattern="*.game"/>
+</mime-type>
+EOF
+sudo update-mime-database /usr/share/mime
