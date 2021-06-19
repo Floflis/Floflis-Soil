b98ffd5423081cc342511732fe662b9b97e55d27
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-06-19 16:51:16 -0300

    to-merge.sh: fully working mimetypes

--- to-merge.sh
+++ to-merge.sh
@@ -406,23 +406,41 @@ application/x-html5			        html5
 application/x-apps			        apps
 application/x-game			        game
 EOF
-$maysudo cat > /usr/share/mime/packages/html5.xml <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="application/x-html5">
-<glob pattern="*.html5"/>
-</mime-type>
+$maysudo cat > /usr/share/mime/packages/x-html5.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns='http://www.freedesktop.org/standards/shared-mime-info'>
+  <mime-type type="application/x-html5">
+    <comment>HTML5 application</comment>
+    <sub-class-of type="text/plain"/>
+    <generic-icon name="application-x-html5"/>
+    <glob pattern="*.html5"/>
+  </mime-type>
+</mime-info>
+
 EOF
-$maysudo cat > /usr/share/mime/packages/apps.xml <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="application/x-apps">
-<glob pattern="*.apps"/>
-</mime-type>
+$maysudo cat > /usr/share/mime/packages/x-apps.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns='http://www.freedesktop.org/standards/shared-mime-info'>
+  <mime-type type="application/x-apps">
+    <comment>Floflis application</comment>
+    <sub-class-of type="application/x-gzip"/>
+    <generic-icon name="application-x-apps"/>
+    <glob pattern="*.apps"/>
+  </mime-type>
+</mime-info>
+
 EOF
-$maysudo cat > /usr/share/mime/packages/game.xml <<EOF
-<?xml version="1.0" encoding="utf-8"?>
-<mime-type xmlns="http://www.freedesktop.org/standards/shared-mime-info" type="application/x-game">
-<glob pattern="*.game"/>
-</mime-type>
+$maysudo cat > /usr/share/mime/packages/x-game.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns='http://www.freedesktop.org/standards/shared-mime-info'>
+  <mime-type type="application/x-game">
+    <comment>Floflis game</comment>
+    <sub-class-of type="application/x-gzip"/>
+    <generic-icon name="application-x-game"/>
+    <glob pattern="*.game"/>
+  </mime-type>
+</mime-info>
+
 EOF
 sudo update-mime-database /usr/share/mime
 
