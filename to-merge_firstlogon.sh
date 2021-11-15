c1363bcb3b4320d996ed965a620cd6eab9a909d7
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-14 22:43:45 -0300

    Install Starship prompt

--- to-merge_firstlogon.sh
+++ to-merge_firstlogon.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "Installing Starship..."
+shit Qmf1XqY9vjU1yHDwEPj3hFBWJqtwGeUyoWPR77kYA7f65D
+echo 'eval "$(starship init bash)"' >> /home/${flouser}/.bashrc
+
+cat >> /home/${flouser}/.config/nu/config.toml <<EOF
+startup = [
+ "mkdir ~/.cache/starship",
+ "starship init nu | save ~/.cache/starship/init.nu",
+ "source ~/.cache/starship/init.nu"
+]
+prompt = "starship_prompt"
+EOF
