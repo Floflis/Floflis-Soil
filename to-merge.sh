835c9f205aabceec5ccfe1155c7876434e059060
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-14 23:13:06 -0300

    Install Uniswap at first boot instead of Cubic

--- to-merge.sh
+++ to-merge.sh
@@ -63,39 +63,6 @@ X-Ubuntu-Gettext-Domain=gnome-menus-3.0
 EOF
 
 $maysudo mkdir /1/apps
-#ipfs pin add $(ipfs dns uniswap.eth)
-ipfs pin add $(ethereal ens contenthash get --domain=uniswap.eth)
-#ipfs add $(ipfs dns uniswap.eth)
-#tmp=$(ipfs pin add $(ethereal ens contenthash get --domain=uniswap.eth) | tr -d "pinned " | tr -d " recursively")
-#ipfs add $tmp
-#ipfs get $(ipfs dns uniswap.eth) --output=/1/apps/uniswap
-ipfs get $(ethereal ens contenthash get --domain=uniswap.eth) --output=/1/apps/uniswap
-# to change: use a variable. test if ipfs dns result starts with /ipfs/, if not use ethereal ens contenthash get --domain=, and if not display an error
-# commands to work on post-install:
-#ipfs add -r /1/apps/uniswap
-#ipfs dns uniswap.eth && ipfs dns uniswap.eth
-#ipfs pin add $(ipfs dns uniswap.eth)
-#ipfs ls $(ipfs dns uniswap.eth)
-#ipfs pin add $(ipfs dns uniswap.eth)
-#- this will have to work on user side (post-install), not only when installing
-$maysudo cat > /usr/bin/uniswap <<EOF
-#!/bin/bash
-
-ipfs-desktop
-xdg-open ipns://uniswap.eth
-EOF
-$maysudo chmod +x /usr/bin/uniswap
-$maysudo cat > /usr/share/applications/uniswap.desktop <<EOF
-[Desktop Entry]
-Encoding=UTF-8
-Name=Uniswap
-Comment=Swap/exchange ETH and tokens
-Type=Application
-Exec=uniswap
-Icon=uniswap
-Categories=Finance;Ethereum;
-Keywords=swap;exchange;tokens;ethereum;
-EOF
 
 $maysudo cat > /usr/bin/decentraland <<EOF
 #!/bin/bash
