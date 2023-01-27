07583bc5f57da61809dc22a93d3d9cd7f7d14c6a
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-27 18:17:18 -0300

    to-merge.sh: FantasqueSansMono font, nushell, Witchcraft Candy Colors and Starship as part of the single "Starshell" package

--- to-merge.sh
+++ to-merge.sh
@@ -1288,15 +1288,6 @@ Keywords=Preferences;Settings;Central;tokens;ethereum;xdai;polygon
 EOF
 # <-- HOME LAYER
 
-echo "Installing FantasqueSansMono font (ComicSans haters gonna hate but its cute <3)..."
-unzip include/Terminal/nerdyfonts/FantasqueSansMono.zip
-$maysudo mv *.ttf *.TTF /usr/share/fonts/truetype/
-#sudo mv *.otf *.OTF /usr/share/fonts/opentype
-#- Font Refresh Tip
-#- After you install new fonts on Ubuntu you’re not able to use them in apps until you reboot. To avoid that, run sudo fc-cache -f -v to refresh the font cache, then logout and back in. After doing this any fonts you installed manually will be selectable in apps/extensions such as this one.
-$maysudo fc-cache -f -v
-#- from https://www.omgubuntu.co.uk/2022/12/desktop-clock-gnome-extension
-
 echo "Installing Sh it..."
 cd include/Tools/shexec
 if [ ! -e .git ]; then git clone --no-checkout https://github.com/Floflis/shit.git .; fi
@@ -1310,65 +1301,17 @@ chmod +x install.sh && $maysudo sh ./install.sh
 sudo apt install curl
 cd "$SCRIPTPATH"
 
-if [ "$flofarch" = "amd64" ]; then
-echo "Installing nushell..."
-tar -xzf include/Terminal/nushell/nu-0.74.0-x86_64-unknown-linux-gnu.tar.gz
-$maysudo mv -f nu-0.74.0-x86_64-unknown-linux-gnu/nu /bin/nu
-$maysudo chmod +x /bin/nu
-rm -rf nu-0.74.0-x86_64-unknown-linux-gnu
-echo "/bin/nu" | $maysudo tee -a /etc/shells
-#-<- should check if line is already added, before re-adding!
-chsh -s /bin/nu
-#echo "Testing if nushell works:"
-#nu
-# introduce in next build
-# task: enable plugins
-fi
-
-echo "Installing Witchcraft Candy Colors..."
-$maysudo apt-get install dconf-cli
-cd include/Terminal/witchcraft-candy-colors
-if [ ! -e .git ]; then git clone --no-checkout https://github.com/Floflis/witchcraft-candy-colors.git .; fi
+echo "Installing the \"Starshell\" package..."
+cd include/Terminal/starshell
+if [ ! -e .git ]; then git clone --no-checkout https://github.com/Floflis/starshell.git .; fi
 if [ -e .git ]; then git pull; fi
 git checkout -f
-chmod +x install.sh && ./install.sh
+chmod +x install.sh && $maysudo sh ./install.sh
 #rm -f install.sh #use noah to exclude everything except .git
-#rm -rf colors
-#rm -rf src
-#rm -f INSTALL.md
-#rm -f LICENSE
-#rm -f readme.md
-#rm -f screenshot.png
+#rm -f README.md
+#rm -f shit
+#rm -f .gitmeta
 cd "$SCRIPTPATH"
-
-echo "Installing Starship..."
-if [ "$flofarch" = "amd64" ]; then
-#shit Qmf1XqY9vjU1yHDwEPj3hFBWJqtwGeUyoWPR77kYA7f65D
-#curl -sS https://starship.rs/install.sh | sh
-#curl -sS https://gateway.pinata.cloud/ipfs/Qmf1XqY9vjU1yHDwEPj3hFBWJqtwGeUyoWPR77kYA7f65D | sh
-#curl -sS https://raw.githubusercontent.com/starship/starship/master/install/install.sh | sh
-tar -xzf include/Terminal/starship/starship-x86_64-unknown-linux-gnu.tar.gz
-$maysudo rm -f /usr/local/bin/starship
-$maysudo mv -f starship /bin/starship
-$maysudo chmod +x /bin/starship
-echo 'eval "$(starship init bash)"' > /home/${flouser}/.bashrc # configure Starship for Bash
-#-<- should check if line is already added, before re-adding!
-
-#cat >> /home/$flouser/.config/mimeapps.list <<EOF
-#
-#EOF
-## this is continuously adding the same entries to mimeapps.list and have to be fixed
-
-cat > /home/${flouser}/.config/nushell/env.nu <<EOF
-mkdir ~/.cache/starship
-starship init nu | save -f ~/.cache/starship/init.nu
-EOF
-echo 'source ~/.cache/starship/init.nu' > /home/${flouser}/.config/nushell/config.nu
-#-
-#https://starship.rs/config/#prompt
-#https://starship.rs/presets/pastel-powerline.html
-fi
-# <---- future task: check against .sha256 file; floflis icons: icon for .sha256 files and file handler for comparing
 
 echo "Installing Hugo (you did great, elder blogspot.com)..."
 if [ "$flofarch" = "386" ]; then
