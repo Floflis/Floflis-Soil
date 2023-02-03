dea2fc6e1058e5b4ea0d82deb4d72bd3ced62293
Daniella Mesquita <mesquitadaniella@proton.me>
2023-02-03 11:13:16 -0300

    to-merge.sh: move pre-installed avatars' functions to own external script

---
+++ include/img/Avatars/install.sh
@@ -0,0 +1,42 @@
+echo "Installing avatars..."
+if [ ! -e /usr/share/cinnamon ]; then $maysudo mkdir /usr/share/cinnamon; fi
+if [ ! -e /usr/share/cinnamon/faces ]; then $maysudo mkdir /usr/share/cinnamon/faces; fi
+if [ ! -e /usr/share/cinnamon/faces/cinnamon ]; then $maysudo mkdir /usr/share/cinnamon/faces/cinnamon; fi
+
+if [ -f /usr/share/cinnamon/faces/0_cars.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_cars.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/0_chess.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_chess.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/0_coffee.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_coffee.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/0_guitar.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_guitar.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/2_10.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_10.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/2_11.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_11.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/2_12.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_12.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/2_13.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_13.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/3_lightning.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_lightning.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/3_mountain.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_mountain.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/3_sky.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_sky.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/3_sunset.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_sunset.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/4_cinnamon.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_cinnamon.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/4_flower.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_flower.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/4_leaf.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_leaf.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/4_sunflower.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_sunflower.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/5_fish.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_fish.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/5_kitten.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_kitten.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/5_penguin.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_penguin.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/5_puppy.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_puppy.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/6_astronaut.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_astronaut.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/6_butterfly.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_butterfly.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/6_flake.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_flake.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/6_grapes.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_grapes.jpg /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_bat.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_bat.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_dog.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_dog.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_elephant.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_elephant.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_fox.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_fox.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_lion.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_lion.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_panda.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_panda.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_penguin.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_penguin.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/7_tucan.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_tucan.png /usr/share/cinnamon/faces/cinnamon; fi
+if [ -f /usr/share/cinnamon/faces/user-generic.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/user-generic.png /usr/share/cinnamon/faces/cinnamon; fi
+
+tar -xzf Avatars.tar.gz
+$maysudo rsync -av Avatars/. /usr/share/cinnamon/faces
+$maysudo rm -rf Avatars
--- to-merge.sh
+++ to-merge.sh
@@ -354,48 +354,8 @@ if [ ! -e /usr/share/wallpapers/FuturePrototype/contents/images ]; then $maysudo
 if [ -f /usr/share/wallpapers/FuturePrototype/gnome-background.xml ]; then $maysudo mv /usr/share/wallpapers/FuturePrototype/gnome-background.xml /usr/share/wallpapers/FuturePrototype/debian/gnome-background.xml; fi
 $maysudo ln -s /1/img/bg.png /usr/share/wallpapers/FuturePrototype/contents/images/1680x1050.png
 
-echo "Installing avatars..."
-if [ ! -e /usr/share/cinnamon ]; then $maysudo mkdir /usr/share/cinnamon; fi
-if [ ! -e /usr/share/cinnamon/faces ]; then $maysudo mkdir /usr/share/cinnamon/faces; fi
-if [ ! -e /usr/share/cinnamon/faces/cinnamon ]; then $maysudo mkdir /usr/share/cinnamon/faces/cinnamon; fi
-#-
-if [ -f /usr/share/cinnamon/faces/0_cars.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_cars.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/0_chess.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_chess.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/0_coffee.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_coffee.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/0_guitar.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/0_guitar.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/2_10.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_10.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/2_11.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_11.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/2_12.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_12.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/2_13.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/2_13.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/3_lightning.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_lightning.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/3_mountain.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_mountain.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/3_sky.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_sky.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/3_sunset.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/3_sunset.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/4_cinnamon.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_cinnamon.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/4_flower.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_flower.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/4_leaf.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_leaf.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/4_sunflower.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/4_sunflower.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/5_fish.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_fish.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/5_kitten.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_kitten.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/5_penguin.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_penguin.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/5_puppy.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/5_puppy.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/6_astronaut.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_astronaut.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/6_butterfly.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_butterfly.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/6_flake.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_flake.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/6_grapes.jpg ]; then $maysudo mv -f /usr/share/cinnamon/faces/6_grapes.jpg /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_bat.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_bat.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_dog.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_dog.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_elephant.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_elephant.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_fox.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_fox.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_lion.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_lion.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_panda.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_panda.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_penguin.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_penguin.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/7_tucan.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/7_tucan.png /usr/share/cinnamon/faces/cinnamon; fi
-if [ -f /usr/share/cinnamon/faces/user-generic.png ]; then $maysudo mv -f /usr/share/cinnamon/faces/user-generic.png /usr/share/cinnamon/faces/cinnamon; fi
-#-
-tar -xzf include/img/Avatars.tar.gz
-$maysudo rsync -av Avatars/. /usr/share/cinnamon/faces
-$maysudo rm -rf Avatars
+# Cinnamon pre-installed avatars
+$maysudo bash include/img/Avatars/install.sh
 
 echo "Installing sounds..."
 if [ ! -e /1/sounds ]; then $maysudo mkdir /1/sounds; fi
