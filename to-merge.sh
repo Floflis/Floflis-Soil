243b06652373b8082077db559cc1ca8b8d955069
Daniella Mesquita <mesquitadaniella@proton.me>
2023-02-04 14:59:36 -0300

    to-merge.sh: resize ubuntu-logo.png while writing it to /usr/share/plymouth/ (so its bigger size issue is fixed)

--- to-merge.sh
+++ to-merge.sh
@@ -521,12 +521,13 @@ cd "$SCRIPTPATH"
 #-
 echo "Installing bootscreen logotype..."
 if [ ! -e /usr/share/plymouth/ubuntu ]; then $maysudo mkdir /usr/share/plymouth/ubuntu; $maysudo mv -f /usr/share/plymouth/ubuntu-logo.png /usr/share/plymouth/ubuntu; fi
-$maysudo cp -f include/img/floflis-logo.png /usr/share/plymouth/ubuntu-logo.png
+$maysudo convert include/img/floflis-logo.png    -resize 512x59  /usr/share/plymouth/ubuntu-logo.png
+#$maysudo cp -f include/img/floflis-logo.png /usr/share/plymouth/ubuntu-logo.png
 $maysudo cp -f include/img/floflis-logo.svg /1/img/ubuntu-logo.svg
 #-
 if [ ! -e /usr/share/plymouth/ubuntucinnamon ]; then $maysudo mkdir /usr/share/plymouth/ubuntucinnamon; $maysudo mv -f /usr/share/plymouth/ubuntucinnamon-logo.png /usr/share/plymouth/ubuntucinnamon; fi
-if [ ! -f /usr/share/plymouth/ubuntucinnamon-logo.png ]; then $maysudo ln -s 'ubuntu-logo.png' /usr/share/plymouth/ubuntucinnamon-logo.png; fi
-#if [ ! -f /usr/share/plymouth/ubuntucinnamon-logo.svg ]; then $maysudo ln -s 'ubuntu-logo.svg' /usr/share/plymouth/ubuntucinnamon-logo.svg; fi
+if [ ! -f /usr/share/plymouth/ubuntucinnamon-logo.png ]; then $maysudo ln -s ubuntu-logo.png /usr/share/plymouth/ubuntucinnamon-logo.png; fi
+#if [ ! -f /usr/share/plymouth/ubuntucinnamon-logo.svg ]; then $maysudo ln -s ubuntu-logo.svg /usr/share/plymouth/ubuntucinnamon-logo.svg; fi
 #-
 if [ ! -e /usr/share/plymouth/themes/ubuntucinnamon-spinner/ubuntu ]; then $maysudo mkdir /usr/share/plymouth/themes/ubuntucinnamon-spinner/ubuntu; $maysudo mv -f /usr/share/plymouth/themes/ubuntucinnamon-spinner/watermark.png /usr/share/plymouth/themes/ubuntucinnamon-spinner/ubuntu; $maysudo ln -s ../../ubuntu-logo.png /usr/share/plymouth/themes/ubuntucinnamon-spinner/watermark.png; fi
 
