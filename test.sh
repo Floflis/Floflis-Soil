1dd48749a4062bbbe31e38cb39dc7e8cd59ae262
Daniella Mesquita <daniellmesquita@protonmail.com>
2023-12-08 17:57:18 -0300

    complement previous commit

--- test.sh
+++ test.sh
@@ -1,4 +1,1 @@
-if [ $(bash /usr/lib/floflis/layers/soil/tools/DEtector.sh) = "gnome" ]
-   then
-      echo "We're on GNOME! Yeah!"
-fi
+if [ $(bash /usr/lib/floflis/layers/soil/tools/DEtector.sh) = "gnome" ]; then echo "We're on GNOME! Yeah!";fi
