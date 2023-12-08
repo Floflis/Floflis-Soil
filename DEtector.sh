fcece427523f3ad8faf1e65b7ff824eaf5b0b642
Daniella Mesquita <daniellmesquita@protonmail.com>
2023-12-08 17:31:29 -0300

    attempt to improve DEtector.sh

--- DEtector.sh
+++ DEtector.sh
@@ -1,8 +1,8 @@
 if [ "$XDG_CURRENT_DESKTOP" = "" ]
 then
   desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
-else
-  desktop=$XDG_CURRENT_DESKTOP
+#else
+#  desktop=$XDG_CURRENT_DESKTOP
 fi
 
 desktop=${desktop,,}  # convert to lower case
