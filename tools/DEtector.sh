3d8a71bc6126d9edd1e24c42b17fa4f4e291c3cd
Daniella Mesquita <daniellmesquita@protonmail.com>
2023-12-09 00:43:32 -0300

    able to detect gnome even in chroot!

--- tools/DEtector.sh
+++ tools/DEtector.sh
@@ -1,10 +1,27 @@
-if [ $(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/') = "" ]
-then
-  desktop=$XDG_CURRENT_DESKTOP
-else
-  desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
-fi
+#if [ $(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/') = "" ]
+#then
+#  desktop=$XDG_CURRENT_DESKTOP
+#else
+#  desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
+#fi
 #-
+
+# checks if a program is installed
+command_exist() {
+    if ! command -v $1 &> /dev/null
+    then
+        echo "Install '$1' and run again."
+        #exit
+    else
+        desktop="gnome"
+    fi
+}
+#use "&>" as in https://stackoverflow.com/a/677212/5623661
+
+# checking if grep and curl exist
+command_exist gnome-shell
+#command_exist curl
+
 desktop=${desktop,,}  # convert to lower case
 echo "$desktop"
 #from https://unix.stackexchange.com/a/116694
