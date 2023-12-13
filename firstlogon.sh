7beb52d8952d503056141815116ce505a30b62f8
Daniella Mesquita <mesquitadaniella@proton.me>
2023-12-13 20:43:32 -0300

    Firstlogon: bonce Core's variable exports to to-merge

--- firstlogon.sh
+++ firstlogon.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export flofmach && export flofdistro && export flofarch && export osfullname && export osname && export osversion && export osbuild && export osbuildcodename && export updatepatch && export year && export layer && export nxtlayer && export distrobase && export user && export specialbuildattempt
+
 flouser=$(jq -r '.name' /1/config/user.json)
 export flouser
+
 bash to-merge_firstlogon.sh
